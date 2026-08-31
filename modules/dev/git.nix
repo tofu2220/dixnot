06f29f0304205647a9842047b00cc6906858540b
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-08-31 15:14:19 +0700

    Configure Git identity

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+
+    config = {
+      user = {
+        name = "tofu2220";
+        email = "66412548+tofu2220@users.noreply.github.com";
+      };
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     gh
