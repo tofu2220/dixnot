8a38a2378c52bc28cce2df84fa970a6024bd043e
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-08-31 18:18:16 +0700

    Configure Sway with Home Manager

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       system = "x86_64-linux";
 
@@ -19,6 +24,15 @@
         ./configuration.nix
         ./modules/apps
         ./modules/dev
+        ./modules/desktop/sway
+
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tofu = import ./home/tofu.nix;
+        }
       ];
     in
     {
---
+++ home/tofu.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  home.username = "tofu";
+  home.homeDirectory = "/home/tofu";
+  home.stateVersion = "26.05";
+
+  xdg.configFile."sway/config".source =
+    ../config/sway/config;
+
+  xdg.configFile."fuzzel/fuzzel.ini".source =
+    ../config/fuzzel/fuzzel.ini;
+}
