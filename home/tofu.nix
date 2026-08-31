45f141fc31674b019023d4c1206b5b6bab8968a4
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-08-31 18:30:27 +0700

    Fix typo

--- home/tofu.nix
+++ home/tofu.nix
@@ -5,9 +5,9 @@
   home.homeDirectory = "/home/tofu";
   home.stateVersion = "26.05";
 
-  xdg.configFile."sway/config".source =
-    ./config/sway/config;
-
-  xdg.configFile."fuzzel/fuzzel.ini".source =
-    ./config/fuzzel/fuzzel.ini;
+  xdg.configFile = {
+    "sway".source = ./config/sway;
+    "fuzzel".source = ./config/fuzzel;
+    "foot".source = ./config/foot;
+  };
 }
