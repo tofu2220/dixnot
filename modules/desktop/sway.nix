e46d8cf221954b5c93de4f51739dbb0a7838fc3b
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-01 08:49:15 +0700

    feat(sway): configure GTK and wlroots desktop portals

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -1,9 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+  };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-wlr
+    ];
   };
 }
