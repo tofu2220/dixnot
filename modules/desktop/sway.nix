221b309ec36f0ec18ac49cd8995067d069ba22bd
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-01 08:54:47 +0700

    feat(sway): configure GTK and wlroots portal backends

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -8,9 +8,19 @@
 
   xdg.portal = {
     enable = true;
+
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-wlr
     ];
+
+    config.sway = {
+      default = [ "gtk" ];
+
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+
+      "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+      "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+    };
   };
 }
