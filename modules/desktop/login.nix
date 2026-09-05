986ecf6c9d139419a6a6dcba680a5456b2a3d636
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-05 14:32:17 +0700

    refactor(desktop): simplify greetd login configuration

--- modules/desktop/login.nix
+++ modules/desktop/login.nix
@@ -9,10 +9,6 @@ let
   tuigreet = lib.getExe pkgs.tuigreet;
   sway = lib.getExe config.programs.sway.package;
   waylandSessions = "${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
-
-  greeting = "HEV SUIT SYSTEMS // AUTHORIZATION REQUIRED";
-
-  theme = "border=yellow;text=yellow;time=yellow;container=black;title=yellow;greet=yellow;prompt=yellow;input=white;action=yellow;button=white";
 in
 {
   services.greetd = {
@@ -28,18 +24,6 @@ in
         "--asterisks"
         "--sessions"
         waylandSessions
-        "--greeting"
-        "'${greeting}'"
-        "--greet-align"
-        "left"
-        "--width"
-        "72"
-        "--window-padding"
-        "2"
-        "--container-padding"
-        "2"
-        "--theme"
-        "'${theme}'"
         "--cmd"
         sway
       ];
